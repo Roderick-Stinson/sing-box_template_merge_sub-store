2e46de95fcb1774e0058de5b943aa8300e49a2f1
Roderick-Stinson <57530721+Roderick-Stinson@users.noreply.github.com>
2025-07-12 23:07:02 +0800

    Update merge.js

--- merge.js
+++ merge.js
@@ -28,7 +28,7 @@ const targetGroups = [
     { tag: "🎥 海外流媒体" },
     { tag: "🗃️ PayPal" },
     { tag: "📟 Telegram" },
-    { tag: "🇺🇸 America" },
+    { tag: "🇺🇸 America", filter: /🇺🇸|美国/i },
 ];
 
 // 7. 给每个 group 分配节点
